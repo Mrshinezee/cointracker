d5662065b51b752f923c7b73511829505ed9abd8
Mrshinezee <ijeomaharinze@gmail.com>
2019-06-16 23:09:45 +0100

    setup redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import store from './redux/reducers/store';
+import NotFound from './components/NotFound';
+import Sample from './components/sample/sample';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Fragment>
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={Sample} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </Fragment>
+    </BrowserRouter>
+  </Provider>
+);
 export default App;
